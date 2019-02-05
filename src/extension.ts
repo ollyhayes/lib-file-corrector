f88410eadf52a9c90ec4b1bbc2ec8b98ae995f9e
Olly Hayes <ohayes@scottlogic.co.uk>
2019-02-05 13:03:54 +0000

    Close the wrong file and open new one

--- src/extension.ts
+++ src/extension.ts
@@ -6,21 +6,74 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "package-definitions" is now active!');
+	// const disposable = vscode.workspace.onDidOpenTextDocument(textDocument => 
+	// better to use window rather than workspace because for workspace it's too early to close the newly opening file
+	const disposable = vscode.window.onDidChangeActiveTextEditor(async textEditor => 
+	{
+		if (textEditor) {
+			const editors = vscode.window.visibleTextEditors.map(editor => editor.document.fileName);
+			console.log(`Document opened: ${textEditor.document.fileName}, Editors: ${editors}`);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+			if (textEditor.document.fileName === "c:\\Users\\ohayes\\tools\\lerna-test\\packages\\library\\src\\library.js") {
+				await vscode.commands.executeCommand("workbench.action.closeActiveEditor");
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+				// next open the correct one
+				vscode.window.showTextDocument()
+
+				// the wrong one will still be present in recent editors though and will show up still in searches and stuff, maybe there's a way to remove that?
+			}
+
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	console.log('Congratulations, your extension "package-definitions" is now active!');
+	 let providerActive = false;
+
+	const provider = vscode.languages.registerDefinitionProvider('javascript', {
+		async provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
+
+			if (providerActive) {
+				return null;
+			}
+			
+			providerActive = true;
+			console.log("definition provider invoked!");
+
+			const definitionLocations = await vscode.commands.executeCommand<vscode.Location[]>('vscode.executeDefinitionProvider', document.uri, position);
+
+			if (!definitionLocations || definitionLocations.length === 0) {
+				return null;
+			}
+
+			const location = definitionLocations[0];
+			const locationPath = location.uri.path;
+
+			const libFolderFinder = /packages\/([^\/]*)\/lib\/(.*)/;
+
+			if (libFolderFinder.test(locationPath)) {
+				const scrPath = locationPath.replace(libFolderFinder, "packages/$1/src/$2");
+
+				const newUri = vscode.Uri.file(scrPath);
+				
+				providerActive = false;
+
+				return [
+					{
+						range: location.range,
+						uri: newUri
+					}
+				];
+			}
+
+			providerActive = false;
+			return definitionLocations;
+		}
+	})
+
+	context.subscriptions.push(provider);
+
+	//vscode.window.showInformationMessage('Hello World!');
+
 }
 
 // this method is called when your extension is deactivated
