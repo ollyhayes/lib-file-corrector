4497aca97ad3259740f2033afeb77cbdb22f2643
Olly Hayes <ohayes@scottlogic.co.uk>
2019-05-14 14:14:47 +0100

    Add commands to disable extension

--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,31 @@ import {basename} from 'path';
 
 let disposable: vscode.Disposable;
 
+const state = {
+	disabled: 0,
+	enabled: 1,
+	disabledOnce: 2
+};
+
 export function activate(context: vscode.ExtensionContext) {
+	let currentState = state.enabled;
 	const libFolderFinder = /packages\/([^\/]*)\/lib\/(.*)/;
 
 	disposable = vscode.window.onDidChangeActiveTextEditor(async textEditor => {
 		if (textEditor && libFolderFinder.test(textEditor.document.uri.path)) {
+
+			switch (currentState) {
+				case state.disabled:
+					vscode.window.setStatusBarMessage('Ignoring lib/src redirection - disabled', 3000);
+					return;
+				case state.enabled:
+					break;
+				case state.disabledOnce:
+					vscode.window.setStatusBarMessage('Ignoring lib/src redirection once', 3000);
+					currentState = state.enabled;
+					return;
+			}
+
 			// close the newly opening incorrect 'lib' version of the file
 			await vscode.commands.executeCommand("workbench.action.closeActiveEditor");
 
@@ -22,14 +42,24 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 
 			// todo:
-			// handle the new file not being found (open the old one again?)
-			// the wrong one will still be present in recent editors list and will show up still in searches and stuff, maybe there's a way to remove that?
-			// find a way of putting the cursor in correct place (using find text maybe)
+			// 		handle the new file not being found (open the old one again?)
+			// 		the wrong one will still be present in recent editors list and will show up still in searches and stuff, maybe there's a way to remove that?
+			// 		find a way of putting the cursor in correct place (using find text maybe)
 		}
 	});
+
+	const stateCommands: [string, number][] = [
+		['lib-file-corrector.disableOnce', state.disabledOnce],
+		['lib-file-corrector.disable', state.disabled],
+		['lib-file-corrector.enable', state.enabled]
+	];
+
+	stateCommands.map(([command, state]) => 
+		context.subscriptions.push(vscode.commands.registerCommand(command, () => currentState = state)));
 }
 
 export function deactivate() {
-	if (disposable)
+	if (disposable) {
 		disposable.dispose();
+	}
 }
